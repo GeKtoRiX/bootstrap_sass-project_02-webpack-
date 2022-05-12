1e2032c765b2737b1a59a1b3c0c53fe1eceb41f8
cb@ndy <gektor93@gmail.com>
2022-05-12 19:53:24 +0300

    added introSection

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import './main.scss';
 var reqScripts = require.context("./scripts", true, /^(.*\.(js$))[^.]*$/i);;
 reqScripts.keys().forEach(reqScripts);
 
+// src's imgs
+import user from './icons/user.png';
+//  project's imgs
+const userImg = document.getElementById('user-box');
+userImg.src = user;
